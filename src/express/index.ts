63325919e13013b5007fc16f3005585fc7ca65b9
OxyTom <oxytom8@gmail.com>
2021-10-03 10:22:23 +0200

    moved public

--- src/express/index.ts
+++ src/express/index.ts
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(GHHandler);
 
-app.use(express.static(__dirname + "/../public"));
+app.use(express.static(__dirname + "/public"));
 
 // GHHandler.on("*", function (event: GHEvent, repo: string, data: IPayload) {
 //   console.log(`Getting ${event}`);
