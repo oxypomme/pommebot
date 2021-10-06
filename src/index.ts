f840f1e0c0fd48b72e79bff570433ee646720122
SUBLET Tom <oxytom8@gmail.com>
2021-10-06 13:31:16 +0200

    feat(modules): :sparkles: First attempt to make modules
    Modules are extensions of pommebot. Like things that are too much specific to be implemented by default

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
 import "./.env";
 import "./bot";
 import "./express";
+import { readdir } from "fs/promises";
+import { IModule } from "./modules/.modules";
+
+(async () => {
+  const modules = await (
+    await readdir(__dirname + "/modules")
+  ).filter((d) => d[0] !== ".");
+
+  for (const module of modules) {
+    (require("./modules/" + module) as IModule).start();
+  }
+})();
